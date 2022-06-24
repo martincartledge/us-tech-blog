a7ecbf8fda6089e72b5e4b2b1bd4843dc5d19f8d
Daniel Ireson <direson@opentable.com>
2022-06-24 15:05:06 +0100

    move into component folders

--- components/Section/index.js
+++ components/Section/index.js
@@ -1,4 +1,4 @@
-import styles from "components/Section.module.css";
+import styles from "components/Section/styles.module.css";
 
 export default function Section({ children }) {
   return <section className={styles.section}>{children}</section>;
