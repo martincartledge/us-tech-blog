aa9554e9846ae31db88e039118afe98eec95a10c
Jeanice-N <jeanice.nguyen@gmail.com>
2022-08-05 15:56:31 +0100

    add posts grid

--- components/PostListSection/index.js
+++ components/PostListSection/index.js
@@ -1,22 +1,11 @@
-import Link from "next/link";
-import { longDate } from "libs/date";
 import Section from "components/Section";
+import PostsGrid from "components/PostsGrid";
 
 export default function PostListSection({ title, posts }) {
   return (
     <Section>
       <h2>{title}</h2>
-      <ul>
-        {posts.map((post) => (
-          <li key={post.slug}>
-            <Link href={`/posts/${encodeURIComponent(post.slug)}`}>
-              <a>
-                {post.title} ({longDate(post.date)}) &rarr;
-              </a>
-            </Link>
-          </li>
-        ))}
-      </ul>
+      <PostsGrid posts={posts} />
     </Section>
   );
 }
