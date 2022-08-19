120567849305436b6c40e87f3176446bba3368be
Daniel Ireson <direson@opentable.com>
2022-08-19 14:55:59 +0100

    add copyright notice

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -3,44 +3,52 @@ import Image from "next/image";
 import Link from "next/link";
 import OTLogo from "statics/OT_logo.png";
 import Container from "components/Container";
-import { SOCIAL_LINKS, LEARN_MORE_LINKS } from "constants/app";
+import { SITE_TITLE, SOCIAL_LINKS, LEARN_MORE_LINKS } from "constants/app";
 
 const Footer = () => (
   <footer className={styles.footer}>
     <Container className={styles.container}>
-      <div className={styles.OTLogo}>
-        <Link href="/">
-          <a>
-            <Image src={OTLogo} alt="OpenTable logo" />
-          </a>
-        </Link>
+      <div className={styles.top}>
+        <div className={styles.OTLogo}>
+          <Link href="/">
+            <a>
+              <Image src={OTLogo} alt="OpenTable logo" />
+            </a>
+          </Link>
+        </div>
+        <div className={styles.socials}>
+          <div className={styles.socialLogos}>
+            {SOCIAL_LINKS.map(({ name, href, image }) => (
+              <Link href={href} key={href}>
+                <a>
+                  <Image
+                    className={styles.socialLogo}
+                    src={image}
+                    alt={`${name} logo`}
+                    width={40}
+                    height={40}
+                  />
+                </a>
+              </Link>
+            ))}
+          </div>
+          <div className={styles.moreLinks}>
+            <span className={styles.moreLinksHeader}>Learn more</span>
+            {LEARN_MORE_LINKS.map(({ href, text }) => (
+              <Link href={href} key={href}>
+                <a className={styles.moreLink}>
+                  <span>{text}</span>
+                </a>
+              </Link>
+            ))}
+          </div>
+        </div>
       </div>
-      <div className={styles.socials}>
-        <div className={styles.socialLogos}>
-          {SOCIAL_LINKS.map(({ name, href, image }) => (
-            <Link href={href} key={href}>
-              <a>
-                <Image
-                  className={styles.socialLogo}
-                  src={image}
-                  alt={`${name} logo`}
-                  width={40}
-                  height={40}
-                />
-              </a>
-            </Link>
-          ))}
-        </div>
-        <div className={styles.moreLinks}>
-          <span className={styles.moreLinksHeader}>Learn more</span>
-          {LEARN_MORE_LINKS.map(({ href, text }) => (
-            <Link href={href} key={href}>
-              <a className={styles.moreLink}>
-                <span>{text}</span>
-              </a>
-            </Link>
-          ))}
-        </div>
+      <div className={styles.bottom}>
+        <span>
+          Copyright © {new Date().getFullYear()} {SITE_TITLE}. All rights
+          reserved.
+        </span>
       </div>
     </Container>
   </footer>
