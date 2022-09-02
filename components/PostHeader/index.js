c3e57cbaa6d89c51a2976bf6f07b351e89c517ff
savithri <savithri0707@gmail.com>
2022-09-02 11:27:02 +0100

    remove unused styles and fix styling issues

--- components/PostHeader/index.js
+++ components/PostHeader/index.js
@@ -1,51 +1,38 @@
-import Link from "next/link";
-import Container from "components/Container";
-import {BLOG_TITLE} from "constants/app";
-import OTLogo from "statics/OT_logo.png";
-import Image from "next/image";
-import heroImage from "statics/postHeroImage.png";
-import postLogo from "statics/OpenTableLogo.svg";
-import styles from "components/PostHeader/styles.module.css";
+import Link from 'next/link';
+import Container from 'components/Container';
+import Image from 'next/image';
 
-export default function PostHeader({post}) {
-    const dateOfPost = new Date(post.date)
+import heroImage from 'statics/postHeroImage.png';
+import postLogo from 'statics/OpenTableLogo.svg';
+
+import { longDate } from 'libs/date';
+import { capitalize } from 'libs/string';
+
+import styles from 'components/PostHeader/styles.module.css';
+
+export default function PostHeader({ post }) {
+    const dateOfPost = longDate(post.date);
     return (
         <Container className={styles.headerContainer}>
             <header className={styles.header}>
-                <div className={styles.navElements}>
-                    <div className={styles.navLogoContainer}>
-                        <div className={styles.navLogo}>
-                            <Link href="/">
-                                <a>
-                                    <Image src={OTLogo} alt="OpenTable logo"/>
-                                </a>
-                            </Link>
-                        </div>
-                        <div className={styles.navLogoText}>
-                        <Link href="/">
-                            <a>
-                                <h1 className={styles.textContent}>{BLOG_TITLE}</h1>
-                            </a>
-                        </Link>
-                        </div>
-                    </div>
-                </div>
                 <div className={styles.headerContents}>
                     <div className={styles.titleContainer}>
+                        <div>{capitalize(post.category)}</div>
                         <div>{post.title}</div>
-                        <p className={styles.postDate}>{dateOfPost && dateOfPost.toDateString()}</p>
+                        <div className={styles.postDetails}>
+                            <p>{`${dateOfPost}`}</p>
+                            <p>{`${post.readingTime} min read`}</p>
+                        </div>
                         <div className={styles.postLogoContainer}>
-                            <Image src={postLogo} alt="opentable logo"/>
+                            <Image src={postLogo} alt="opentable logo" />
                             <div className={styles.textContent}>
                                 <p>OpenTable</p>
                                 <p className={styles.teamName}>Tech Team</p>
                             </div>
                         </div>
-
                     </div>
                     <div className={styles.heroImage}>
-                        <Image src={heroImage} alt="heroImage"/>
-
+                        <Image src={heroImage} alt="heroImage" />
                     </div>
                 </div>
 
