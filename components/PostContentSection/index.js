e32b85c859588cb951f09a035db4b21f526c1a9b
Daniel Ireson <direson@opentable.com>
2022-09-11 13:37:33 +0100

    remove redundant post metadata

--- components/PostContentSection/index.js
+++ components/PostContentSection/index.js
@@ -1,12 +1,9 @@
-import { longDate } from "libs/date";
 import Section from "components/Section";
 import styles from "./styles.module.css";
 
 export default function PostContentSection({ post }) {
   return (
     <Section>
-      <h1>{post.title}</h1>
-      <p>{longDate(post.date)}</p>
       <div
         className={styles.content}
         dangerouslySetInnerHTML={{ __html: post.html }}
