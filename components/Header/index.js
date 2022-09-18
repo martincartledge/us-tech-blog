0443a9053415564ba4ea83e516ba386fa61d8314
Daniel Ireson <direson@opentable.com>
2022-09-18 15:07:48 +0300

    extract title and subtitle components

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,14 +1,16 @@
 import Container from "components/Container";
-import styles from "components/Header/styles.module.css";
 import Section from "components/Section";
+import Title from "components/Title";
+import Subtitle from "components/Subtitle";
+import styles from "components/Header/styles.module.css";
 
 export default function Header({ title, subtitle }) {
   return (
     <div className={styles.wrapper}>
       <Container>
         <Section>
-          <h1 className={styles.title}>{title}</h1>
-          {subtitle && <h2 className={styles.subtitle}>{subtitle}</h2>}
+          <Title value={title} />
+          {subtitle && <Subtitle value={subtitle} />}
         </Section>
       </Container>
     </div>
