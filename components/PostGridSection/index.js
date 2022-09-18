6c4f2a611a26b460d23f4bfd53efe1a1162d6f64
Daniel Ireson <direson@opentable.com>
2022-09-18 15:46:28 +0300

    add view more posts link

--- components/PostGridSection/index.js
+++ components/PostGridSection/index.js
@@ -1,12 +1,21 @@
+import Link from "next/link";
 import Container from "components/Container";
 import Section from "components/Section";
 import PostsGrid from "components/PostsGrid";
+import styles from "components/PostGridSection/styles.module.css";
 
-export default function PostGridSection({ posts }) {
+export default function PostGridSection({ posts, viewMoreLink = false }) {
   return (
     <Container>
       <Section>
         <PostsGrid posts={posts} />
+        {viewMoreLink && (
+          <div className={styles.linkContainer}>
+            <Link href="/posts">
+              <a className={styles.link}>View more posts</a>
+            </Link>
+          </div>
+        )}
       </Section>
     </Container>
   );
