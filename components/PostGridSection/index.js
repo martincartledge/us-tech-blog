fe1db6035c3a647a0c5bd2c35ef4f6e88bf54f13
Daniel Ireson <direson@opentable.com>
2022-09-19 12:24:01 +0300

    add back to top link

--- components/PostGridSection/index.js
+++ components/PostGridSection/index.js
@@ -4,7 +4,11 @@ import PostsGrid from "components/PostsGrid";
 import LinkButton from "components/LinkButton";
 import styles from "components/PostGridSection/styles.module.css";
 
-export default function PostGridSection({ posts, viewMoreLink = false }) {
+export default function PostGridSection({
+  posts,
+  viewMoreLink = false,
+  backToTopLink = false,
+}) {
   return (
     <Container>
       <Section>
@@ -12,6 +16,11 @@ export default function PostGridSection({ posts, viewMoreLink = false }) {
         {viewMoreLink && (
           <div className={styles.linkContainer}>
             <LinkButton href="/posts" text="View more posts" />
+          </div>
+        )}
+        {backToTopLink && (
+          <div className={styles.linkContainer}>
+            <LinkButton href="/posts" text="Back to top" />
           </div>
         )}
       </Section>
