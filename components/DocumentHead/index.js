b431f760cf6362200c6b7bb8902968ad11c04015
Daniel Ireson <direson@opentable.com>
2022-09-28 19:06:14 +0100

    add opengraph tags

--- components/DocumentHead/index.js
+++ components/DocumentHead/index.js
@@ -2,10 +2,26 @@ import Head from "next/head";
 import { SITE_TITLE } from "constants/app";
 
 export default function DocumentHead({ title, description }) {
+  const htmlTags = (
+    <>
+      <title>{title ? `${title} | ${SITE_TITLE}` : SITE_TITLE}</title>
+      {description && <meta name="description" content={description} />}
+    </>
+  );
+
+  const openGraphTags = (
+    <>
+      <meta property="og:title" content={title ?? SITE_TITLE} />
+      {description && <meta name="og:description" content={description} />}
+      <meta property="og:site_name" content={SITE_TITLE} />
+      <meta property="og:type" content="website" />
+    </>
+  );
+
   return (
     <Head>
-      <title>{title ? title + " | " + SITE_TITLE : SITE_TITLE}</title>
-      {description && <meta name="description" content={description} />}
+      {htmlTags}
+      {openGraphTags}
     </Head>
   );
 }
